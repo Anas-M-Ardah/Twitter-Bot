9aad1dc4ce1928d03b4bd0c9c85cf0c93038c07f
Mrrebhi <anas.m.ardah@gmail.com>
2023-08-13 19:41:34 +0300

    V.5

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const tweet = async () => {
     const author = quote.author;
     let secondParameter = '';
     if (author !== 'Anonymous') {
-      secondParameter = '\nauthor: ' + author;
+      secondParameter = '\n-' + author + '\n#quote #dailyquote #quotes #wisdom #inspiration #inspirational';
     }
     console.log('Sending Tweet');
     await twitterClient.v2.tweet(quote.content + secondParameter);
