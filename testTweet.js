e26e02e02619fd712217549a43009b9cb1768b37
unknown <anas.m.ardah@gmail.com>
2024-11-09 21:08:55 +0300

    app rework

--- testTweet.js
+++
@@ -1,6 +0,0 @@
-const { handler } = require('./scheduledTweet');
-
-exports.handler = async (event, context) => {
-  console.log('Testing scheduled tweet function');
-  return handler(event, context);
-};
