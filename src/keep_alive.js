a7280a5141c8bb2fb2fef019dc1f4b756f3aa6e3
Anas Ardah <anas.m.ardah@gmail.com>
2025-02-09 16:38:22 +0300

    testing

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,6 +1,16 @@
-const http = require('http');
+const axios = require('axios');
 
-http.createServer((req, res) => {
-    res.write("I'm alive!");
-    res.end();
-}).listen(3000);
+// Add this to your application
+function keepAlive() {
+    const url = "https://twitter-bot-y1c3.onrender.com";
+    setInterval(async () => {
+        try {
+            const response = await axios.get(url);
+            console.log('Keep-alive ping sent');
+        } catch (error) {
+            console.error('Keep-alive ping failed:', error);
+        }
+    }, 10); // 14 minutes (Render free tier sleeps after 15 minutes of inactivity)
+}
+
+keepAlive();
